1c64dce188db4f57ef37c69da4e909e08f44402d
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-06-16 11:16:22 -0700

    Initial commit

---
+++ lib/src/main.dart
@@ -0,0 +1,23 @@
+String fullName = 'Jack Bauer';
+
+greet (){
+  print(fullName);
+}
+
+main(){
+
+  int marks = 100;
+  var name = 'Ketu Imevbore';
+  var name1 = "Ket'u Imevbore";
+  var isWorking = true;
+  var isNotWorking = false;
+  greet();
+
+
+  print(marks);
+  print(name);
+  print(name1);
+  print(isWorking);
+  print(isNotWorking);
+
+}
